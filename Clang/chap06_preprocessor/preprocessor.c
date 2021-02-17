f2b2281644f60d4052eb9788c4da6402efd91a0c
xengom <xengom@gmail.com>
2021-02-17 16:44:29 +0900

    chap06 add

--- Clang/chap06_preprocessor/preprocessor.c
+++ Clang/chap06_preprocessor/preprocessor.c
@@ -1,5 +1,31 @@
 /*
-#include <stdio.h> //라이브러리 형태로 제공
-#include "printx.h" // 프로젝트에 포함된 파일
-
+전처리기 예제
 */
+
+#include <stdio.h>
+#define PI 3.141592
+
+#define POWA(x) (x*x)
+#define CIRCLE_AREA(r) (PI * POWA(r))
+#define CIRCLE_ROUND(r) (PI * 2 * r)
+
+float circle_area(float r1);
+float circle_round(float r2);
+
+void main() {
+	float a;
+	printf("input r as float:");
+	scanf_s("%f", &a);
+	printf("when r is %f, circle's area is %f\n", a, circle_area(a));
+	printf("when r is %f, circle's round is %f\n", a, circle_round(a));
+	printf("when r is %f, circle's area is %f\n", a, CIRCLE_AREA(a));
+	printf("when r is %f, circle's round is %f\n", a, CIRCLE_ROUND(a));
+}
+
+float circle_area(float r1) {
+	return PI * POWA(r1);
+}
+
+float circle_round(float r2) {
+	return 2 * PI * r2;
+}
