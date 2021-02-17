67a22620005e8ee42b3082449838233013e7cff4
xengom <xengom@gmail.com>
2021-02-17 14:50:14 +0900

    .

---
+++ clang/Clang/sysinfo.c
@@ -0,0 +1,11 @@
+#include "stdio.h"
+
+int main() {
+	printf(">>> system infoormation <<<\n");
+	printf("char size : %d \n",		sizeof(char));
+	printf("short size : %d \n",	sizeof(short));
+	printf("int size : %d \n",		sizeof(int));
+	printf("long size : %d \n",		sizeof(long));
+	printf("float size : %d \n",	sizeof(float));
+	printf("double size : %d \n",	sizeof(double));
+}
