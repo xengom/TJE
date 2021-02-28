bb6a455462f619b6c0e3d1dfe53536329c7a6973
xengom <xengom@gmail.com>
2021-02-28 15:56:38 +0900

    실습숙제배열추가

--- 실습문제/02.배열(숙제_3x4_to_4x3).c
+++ 실습문제/02.배열(숙제_3x4_to_4x3).c
@@ -31,7 +31,6 @@ int change(){
 			if(j==2) printf("\n");
 		}
 	}
-
 	
 }
 
@@ -45,17 +44,37 @@ int change2(){
 		}
 	}
 	
-	int arr2[4][3];
+	int arr2[4][3]={0,};
+	int cnt;//줄바뀌는 숫자 개수 겸 행 번호 
+	int cnt2=1;//옆으로 미는 숫자 개수 
+	for(cnt=3;cnt>=0;cnt--){
+		if(cnt!=0){
+			for(i=0;i<cnt;i++){
+				arr2[cnt][i]=arr[cnt-1][i+4-cnt];
+			}
+			if(cnt2<3){
+				for(j=0;j<cnt2;j++){
+					arr2[cnt-1][j+3-cnt2]=arr[cnt-1][j];
+				}
+			}
+			cnt2++;
+		}
+		else{
+			for(k=0;k<3;k++){
+				arr2[cnt][k]=arr[cnt][k];
+			}
+		}
+	}
+		
+	
+
 	for(i=0;i<4;i++){
 		for(j=0;j<3;j++){
-			
-			
-				
 			printf("%5d",arr2[i][j]);
 			if(j==2) printf("\n");
 		}
 	}
-
+	
 	
 }
 
