9166f50d9f1f25297d6fc5592a129c37efb3a82b
xengom <xengom@gmail.com>
2021-05-11 14:27:14 +0900

    참고정리 추가

--- SampleSTS/src/main/java/com/tj/h/SampleMain.java
+++ SampleSTS/src/main/java/com/tj/h/SampleMain.java
@@ -5,15 +5,14 @@ import org.springframework.context.support.GenericXmlApplicationContext;
 
 public class SampleMain {
 	  public static void main(String[] args) {
-	     //1. XML 경로 파악
-	     String configlocation = "classpath:applicationCTX.xml";
-	     //2. Abs 인터페이스 통해 저장한 파일을 ctx에 담음
-	     AbstractApplicationContext ctx=
-	           new GenericXmlApplicationContext(configlocation);
-	     //3.getBean을 이용해 ctx에 담겨있는 정보들을 sc객체에 제공
-	     SampleCal sc = ctx.getBean("sampleCal",SampleCal.class);
-	     
-	    sc.add();
-	    sc.multi();
+		//1. XML 경로 파악
+		     String configlocation = "classpath:applicationCTX.xml";
+		     //2. Abs 인터페이스 통해 저장한 파일을 ctx에 담음
+		     AbstractApplicationContext ctx=
+		           new GenericXmlApplicationContext(configlocation);
+		     //3.getBean을 이용해 ctx에 담겨있는 정보들을 sc객체에 제공
+		    SampleScore ss = ctx.getBean("sampleScore",SampleScore.class);
+		    ss.print();
+	    
 	}
 }
